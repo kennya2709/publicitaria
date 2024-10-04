18e05c5012d05eb273ba707023fc4f4632c21b74
Guayaba15 <daviddavilabarrios@gmail.com>
2024-10-04 13:08:31 -0600

    android-added

---
+++ android/app/src/main/java/com/bytebridge/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bytebridge.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bytebridge.app',
+  appName: 'ByteBridge Ads',
+  webDir: 'build'
+};
+
+export default config;
